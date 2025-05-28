55bea69dff816def22349198d60e3fcc6910f420
celsomsilva <celsomsilva@gmail.com>
2025-05-28 17:26:14 -0300

    Updating files

--- coursera-2017/assignment/complete.R
+++ coursera-2017/assignment/complete.R
@@ -1,18 +1,18 @@
 complete <- function(directory, id = 1:332){
   
   df <- data.frame(id = numeric(), nobs = numeric())
-  names(df)<-c("id","nobs")
-   
+  
   for (i in id) {
     #formatting the numbers 
-    name <- sprintf("%03d",i)
+    name <- sprintf("%03d", i)
     #formatting file names
-    file <- paste(name, ".csv", sep="")
-    myData <- read.csv(paste(directory, file, sep="/"))
-    noNa <- myData[complete.cases(myData),]
+    file <- paste0(name, ".csv")
+    myData <- read.csv(file.path(directory, file))
+    noNa <- myData[complete.cases(myData), ]
     row <- nrow(noNa)
     df <- rbind(df, data.frame(id = i, nobs = row))
   }
   
   df
 }
+
